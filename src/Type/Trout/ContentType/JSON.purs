f0d8ab4aeef558b847fb7327054339cd9cce1ba6
Jaye Marshall <marshall.jaye@gmail.com>
2020-05-07 14:23:26 +0100

    fix json mimetype

--- src/Type/Trout/ContentType/JSON.purs
+++ src/Type/Trout/ContentType/JSON.purs
@@ -1,19 +1,15 @@
 module Type.Trout.ContentType.JSON where
 
 import Prelude
+
 import Data.Argonaut (class DecodeJson, class EncodeJson, decodeJson, encodeJson)
 import Data.Argonaut.Core (stringify)
 import Data.Argonaut.Parser (jsonParser)
+import Data.Map (Map, empty)
 import Data.MediaType.Common (applicationJSON)
 import Data.Tuple (Tuple(..))
-import Type.Trout.ContentType
-  ( class AllMimeRender
-  , class HasMediaType
-  , class MimeParse
-  , class MimeRender
-  , getMediaType
-  , mimeRender
-  )
+import Data.Tuple.Nested (type (/\), (/\))
+import Type.Trout.ContentType (class AllMimeRender, class HasMediaType, class MimeParse, class MimeRender, getMediaType, mimeRender)
 
 -- | A content type, corresponding to the `application/json` media type.
 data JSON
@@ -21,11 +17,11 @@ data JSON
 instance hasMediaTypeJson :: HasMediaType JSON where
   getMediaType _ = applicationJSON
 
-instance mimeRenderJson :: EncodeJson a => MimeRender a JSON String where
-  mimeRender _ = stringify <<< encodeJson
+instance mimeRenderJson :: EncodeJson a => MimeRender a JSON ((Map String String) /\ String) where
+  mimeRender _ = ((/\) empty) <<< stringify <<< encodeJson
 
 instance mimeParseJson :: DecodeJson a => MimeParse String JSON a where
   mimeParse _ = jsonParser >=> decodeJson
 
-instance allMimeRenderJson :: EncodeJson a => AllMimeRender a JSON String where
+instance allMimeRenderJson :: EncodeJson a => AllMimeRender a JSON ((Map String String) /\ String) where
   allMimeRender p x = pure (Tuple (getMediaType p) (mimeRender p x))
